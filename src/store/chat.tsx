2afaac21a916dd8d567e610168683efd619f1091
DuetoPark <inhye.park.dev@gmail.com>
2024-11-06 16:33:44 +0900

    ♻️ [refactor] 사용하지 않는 전역 상태 삭제

--- src/store/chat.tsx
+++ src/store/chat.tsx
@@ -1,35 +1,12 @@
 import { create } from 'zustand';
 
-import { CHAT_LIST_INIT } from '@/pages/chat/constants';
-import type { ChatDataType } from '@/models/chat';
-
 type ChatType = {
   click: boolean;
-  chatList: ChatDataType[] | null;
-  setChatList: (chatData: ChatDataType[] | ChatDataType) => void;
   setClick: () => void;
 };
 
 const useChatStore = create<ChatType>()((set) => ({
   click: false,
-  chatList: CHAT_LIST_INIT,
-  setChatList: (chatData) =>
-    set((state) => {
-      if (state.chatList) {
-        if (Array.isArray(chatData)) {
-          return { chatList: [...state.chatList, ...chatData] };
-        }
-
-        return { chatList: [...state.chatList, chatData] };
-      }
-
-      if (Array.isArray(chatData)) {
-        return { chatList: chatData };
-      }
-
-      return { chatList: [chatData] };
-    }),
-
   setClick: () => set((state) => ({ click: !state.click })),
 }));
 
