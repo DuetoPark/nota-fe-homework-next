39f59db1826d1ceadd44dac51b51994f55f705ed
DuetoPark <inhye.park.dev@gmail.com>
2024-11-08 01:30:40 +0900

    💄 [style] UI 스타일 구현 - ChatBoxSection 컴포넌트

--- src/pages/chat/components/ChatBoxSection/index.tsx
+++ src/pages/chat/components/ChatBoxSection/index.tsx
@@ -1,20 +1,28 @@
 import { HTMLAttributes } from 'react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import classNames from 'classnames/bind';
+import ScrollToBottom from 'react-scroll-to-bottom';
 import useChatData from '@/hooks/useChatData';
 import { useChatStore } from '@/store/chat';
+import useChatId from '@/hooks/useChatId';
 import { MODEL_ID_INIT, MODEL_ID_NEW } from '../../constants';
 
 import Button from '@/components/atoms/Button';
 import Textarea from '@/components/atoms/Textarea';
+import Loading from '@/components/query/Loading';
 import ModelSelect from './ModelSelect';
-import Dialoguelist from './DialogList';
-import useChatId from '@/hooks/useChatId';
+import Dialoguelist from './DialogueList';
 
-type SectionPropsType = HTMLAttributes<HTMLDivElement>;
+import styles from './chatBoxSection.module.css';
 
-const ChatBoxSection = ({ ...props }: SectionPropsType) => {
+interface SectionPropsType extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+const cx = classNames.bind(styles);
+
+const ChatBoxSection = ({ className, ...props }: SectionPropsType) => {
   const navigate = useNavigate();
   const { chatId } = useChatId();
   const { click } = useChatStore();
@@ -42,41 +50,49 @@ const ChatBoxSection = ({ ...props }: SectionPropsType) => {
   }, []);
 
   return (
-    <section {...props}>
-      <h2>ChatBox</h2>
+    <section className={cx('section', className)} {...props}>
+      <header className={cx('header')}>
+        <h2 className="visually-hidden">채팅창</h2>
 
-      {modelsQueryIsLoading && <p>모델 리스트를 호출중입니다.</p>}
-      {modelsQuery && (
-        <ModelSelect
-          modelList={modelsQuery}
-          modelId={model}
-          onSelectChange={(selectedModelId) => {
-            navigate('/');
-            setModel(selectedModelId);
+        {modelsQueryIsLoading && <Loading text="모델 리스트을(를) 불러오는 중입니다." />}
+        {modelsQuery && (
+          <ModelSelect
+            modelList={modelsQuery}
+            modelId={model}
+            onSelectChange={(selectedModelId) => {
+              navigate('/');
+              setModel(selectedModelId);
+            }}
+          />
+        )}
+      </header>
+
+      <ScrollToBottom className={cx('content')}>
+        {chatIsLoading && <Loading text="대화 내역을(를) 불러오는 중입니다." />}
+        {!chatIsLoading && dialogues && <Dialoguelist dialogueList={dialogues} />}
+        {mutationDialogue.isPending && <Loading text={`${message}에 대한 답변을 생성중입니다.`} />}
+      </ScrollToBottom>
+
+      <footer className={cx('footer')}>
+        <Textarea
+          className={cx('textarea')}
+          value={message}
+          onTextInput={(message) => {
+            setMessage(message);
           }}
+          disabled={!model}
+          placeholder="메세지를 입력하세요"
         />
-      )}
 
-      {chatIsLoading && <p>대화 내역을 호출중입니다.</p>}
-      {dialogues && <Dialoguelist dialogueList={dialogues} />}
-      {mutationDialogue.isPending && <p>"{message}"에 대한 답변을 생성중입니다.</p>}
-
-      <Textarea
-        value={message}
-        onTextInput={(message) => {
-          setMessage(message);
-        }}
-        disabled={!model}
-        placeholder="메세지를 입력하세요"
-      />
-
-      <Button
-        type="button"
-        onClick={addNewDialogue}
-        disabled={!model || mutationDialogue.isPending}
-      >
-        {mutationDialogue.isPending ? '답변 생성중...' : '프롬프트 보내기'}
-      </Button>
+        <Button
+          type="button"
+          className={cx('submit-button')}
+          onClick={addNewDialogue}
+          disabled={!model || mutationDialogue.isPending}
+        >
+          {mutationDialogue.isPending ? '답변 생성중...' : '프롬프트 보내기'}
+        </Button>
+      </footer>
     </section>
   );
 };
