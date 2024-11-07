d1d9fdca43ca92f0eda7568f41ea6d2bec23f718
DuetoPark <inhye.park.dev@gmail.com>
2024-11-07 17:59:07 +0900

    💄 [style] UI 스타일 구현 - ChatListSection 컴포넌트

--- src/pages/chat/components/ChatListSection/index.tsx
+++ src/pages/chat/components/ChatListSection/index.tsx
@@ -1,39 +1,54 @@
 import { HTMLAttributes } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { CiViewList } from 'react-icons/ci';
 import useChatsQuery from '@/hooks/queries/useChatsQuery';
 import { useChatStore } from '@/store/chat';
 
 import AddChatButton from './AddChatButton';
 import ChatList from './ChatList';
+import classNames from 'classnames/bind';
 
-type SectionPropsType = HTMLAttributes<HTMLDivElement>;
+import styles from './chatListSection.module.css';
 
-const ChatListSection = ({ ...props }: SectionPropsType) => {
+interface SectionPropsType extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+const cx = classNames.bind(styles);
+
+const ChatListSection = ({ className, ...props }: SectionPropsType) => {
   const navigate = useNavigate();
   const { setClick } = useChatStore();
   const { isLoading, data: chatsQuery } = useChatsQuery();
 
   return (
-    <section {...props}>
-      <h2>ChatList</h2>
+    <section className={cx('section', className)} {...props}>
+      <header className={cx('header')}>
+        <h2 className={cx('title')}>
+          <CiViewList aria-hidden />
+          채팅 목록
+        </h2>
 
-      <AddChatButton
-        className=""
-        onClick={() => {
-          navigate('/');
-          setClick();
-        }}
-      />
-
-      {isLoading && <p>채팅 리스트를 불러오는 중입니다.</p>}
-      {chatsQuery && (
-        <ChatList
-          chatList={chatsQuery}
-          onClick={(currentId) => {
-            navigate(`/${currentId}`);
+        <AddChatButton
+          className=""
+          onClick={() => {
+            navigate('/');
+            setClick();
           }}
         />
-      )}
+      </header>
+
+      <div className={cx('content')}>
+        {isLoading && <p>채팅 리스트를 불러오는 중입니다.</p>}
+        {chatsQuery && (
+          <ChatList
+            chatList={chatsQuery}
+            onClick={(currentId) => {
+              navigate(`/${currentId}`);
+            }}
+          />
+        )}
+      </div>
     </section>
   );
 };
