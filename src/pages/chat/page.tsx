eb749fafa11891877139f1cfd166f8f0f9698b0e
DuetoPark <inhye.park.dev@gmail.com>
2024-11-08 12:45:07 +0900

    🐛 [error] Chat 페이지 chatId 변경 - 전역상태 > useParams

--- src/pages/chat/page.tsx
+++ src/pages/chat/page.tsx
@@ -1,22 +1,22 @@
 import classNames from 'classnames/bind';
-import { useShallow } from 'zustand/shallow';
-import { useChatStore } from '@/store/chat';
 
 import ChatListSection from './components/ChatListSection';
 import ChatBoxSection from './components/ChatBoxSection';
 import Container from '@/components/layout/Container';
 
 import styles from './page.module.css';
+import { useParams } from 'react-router-dom';
+import { CHAT_ID_INIT } from './constants';
 
 const cx = classNames.bind(styles);
 
 const Chat = () => {
-  const { chatId } = useChatStore(useShallow((state) => state));
+  const { chatId } = useParams();
 
   return (
     <Container className={cx('container')}>
-      <ChatListSection chatId={chatId} className={cx('box', 'list-section')} />
-      <ChatBoxSection chatId={chatId} className={cx('box', 'box-section')} />
+      <ChatListSection chatId={chatId ?? CHAT_ID_INIT} className={cx('box', 'list-section')} />
+      <ChatBoxSection chatId={chatId ?? CHAT_ID_INIT} className={cx('box', 'box-section')} />
     </Container>
   );
 };
